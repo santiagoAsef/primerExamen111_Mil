cbb832803d0908ab46f58439213d64722f20ff7a
Jose Luis <josebeas24@outlook.com>
2018-05-10 19:59:45 -0300

    cambios

--- Examen/src/examen/Examen.java
+++ Examen/src/examen/Examen.java
@@ -12,9 +12,13 @@ public class Examen {
         Cliente santiago= new Cliente("santiago", 35966753);
         Orden miOrden=new Orden(1, Date.from(Instant.EPOCH));
         Notebook miCompu=new Notebook("compac", "presario", "intel ci5", 10000024);
+        Notebook miCompu2=new Notebook("compac", "presario", "intel ci5", 1000000024);
         DetalleOrden miDetalle= new DetalleOrden(miCompu, 10000, 1);
+        DetalleOrden miDetalle2= new DetalleOrden(miCompu2, 10006, 2);
         miOrden.agregarItem(miDetalle);
+        miOrden.agregarItem(miDetalle2);
         santiago.agregarOrden(miOrden);
+        
         
     }
     
